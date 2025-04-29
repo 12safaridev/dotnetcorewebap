eb4af68a4c64b248d73b4b1928166f6d431c63e5
Outlook Davinder <davinder.bhagat@outlook.com>
2025-04-29 08:26:20 +0530

    Machine name will be diplayed in case of error

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // builder.Services.AddMvc();
+
+builder.Services.AddProblemDetails(options =>
+{
+    //options.IncludeExceptionDetail = (ctx, ex) => true;
+    options.CustomizeProblemDetails = ctx =>
+    {
+        ctx.ProblemDetails.Extensions.Add("Server", Environment.MachineName);
+    };
+});
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
